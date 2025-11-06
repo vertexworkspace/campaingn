47ee8cf5df6d1b88f65154bb41083dc60d21fc43
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-11-06 18:38:40 +0530

    build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 import Navbar from "@/components/sections/Navbar";
-import Footer from "@/components/sections/Footer";
 import SmoothScroll from "@/components/SmoothScroll";
 
 const notoSans = Noto_Sans({
@@ -54,9 +54,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={notoSans.className}>
-        <SpeedInsights />
         <Navbar />
-        <SmoothScroll>{children}</SmoothScroll>
+        <SmoothScroll>   <SpeedInsights />{children}</SmoothScroll>
       </body>
     </html>
   );
