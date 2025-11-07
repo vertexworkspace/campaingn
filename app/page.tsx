e3f62df56be81bdd952210bb17b41e9ff821b0c2
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-11-07 21:12:43 +0530

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,5 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Page() {
-  const router = useRouter();
-
-  useEffect(() => {
-    let navigated = false;
-
-    // Try going back first
-    router.back();
-
-    // After a short delay, if still on the same page, redirect
-    const timeout = setTimeout(() => {
-      if (!navigated) {
-        router.replace("/coworking-space");
-      }
-    }, 300);
-
-    // Clean up
-    return () => clearTimeout(timeout);
-  }, [router]);
-
-  return null;
+ <div></div>
 }
