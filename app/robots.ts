475d35e6eb0232a85d66ba4a62701c78bd5c61ea
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-11-18 11:46:38 +0530

    sitemapFix

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://vertexworkspace.com";
+  const baseUrl = "https://bookings.vertexworkspace.com";
 
   return {
     rules: [
