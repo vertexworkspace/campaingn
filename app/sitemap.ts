5d7077ff1ff6af74008c3dc6fd2603419c1e7741
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-11-18 11:49:24 +0530

    sitemap updation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,28 +11,16 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1.0,
     },
     {
-      url: `${baseUrl}/campaign`,
+      url: `${baseUrl}/private-offices`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.9,
     },
     {
-      url: `${baseUrl}/private-offices`,
+      url: `${baseUrl}/vertex-solutions`,
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/about`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.6,
-    },
-    {
-      url: `${baseUrl}/contact`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.6,
-    },
+      priority: 0.9,
+    }
   ];
 }
